3382de7b2d4d6941ffe38e080e8bd433b5e60245
Dishank Bishnoi <90502552+dishankbishnoi@users.noreply.github.com>
2025-03-26 22:27:19 +0530

    Made some final changes with core programming

---
+++ src/java_strings/level1_problems/NullPointerDemo.java
@@ -0,0 +1,23 @@
+package java_strings.level1_problems;
+
+public class NullPointerDemo {
+    public static void main(String[] args) {
+        generateException();
+        handleException();
+    }
+
+    public static void generateException() {
+        String text = null;
+        // This will throw NullPointerException
+        System.out.println(text.length());
+    }
+
+    public static void handleException() {
+        try {
+            String text = null;
+            System.out.println(text.length());
+        } catch (NullPointerException e) {
+            System.out.println("Caught NullPointerException: " + e.getMessage());
+        }
+    }
+}
